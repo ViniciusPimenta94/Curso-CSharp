0f32a54be2e885446fe0bcfa3b11cf2b962d7e77
ViniciusPimenta94 <99184817+ViniciusPimenta94@users.noreply.github.com>
2023-10-18 10:11:50 -0300

    Add files via upload

---
+++ Seção 5/Ex/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex {
+    class Program {
+        static void Main(string[] args) {
+            ContaBancaria conta;
+
+            Console.Write("Entre o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre o titular da conta: ");
+            string titular = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char resp = char.Parse(Console.ReadLine());
+
+            if (resp.ToString().ToLower() == "s") {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                conta = new ContaBancaria(numero, titular, depositoInicial);
+            }
+            else {
+                conta = new ContaBancaria(numero, titular);
+            }
+
+            Console.WriteLine($"\nDados da conta: \n{conta}");
+
+            Console.Write("\nEntre um valor para depósito: ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(quantia);
+
+            Console.WriteLine($"\nDados da conta atualizados: \n{conta}");
+
+            Console.Write("\nEntre um valor para saque: ");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(quantia);
+
+            Console.WriteLine($"\nDados da conta atualizados: \n{conta}");
+        }
+    }
+}
